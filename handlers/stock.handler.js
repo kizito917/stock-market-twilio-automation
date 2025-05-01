f7fa6b596e2a1415d0caca7a78f5970a6c04c5aa
kizito917 <johnkingsley917@gmail.com>
2025-05-01 08:56:15 +0100

    Stock handler refactored.

--- handlers/stock.handler.js
+++ handlers/stock.handler.js
@@ -2,8 +2,8 @@ async function getMarketPrices(symbol) {
     const marketPrice = await getStockMarketCurrentPrice(symbol);
     const volatilityResult = await getVolatilityIndexData();
     
-    console.log(marketPrice);
-    console.log(volatilityResult);
+    console.log("Market price result: ", marketPrice);
+    console.log("Volatility result: ", volatilityResult);
 
     return {
         marketPrice,
@@ -19,7 +19,7 @@ async function getVolatilityIndexData() {
         }
 
         const result = await response.json();
-        console.log("Result from Volatility index", JSON.stringify(result));
+        //console.log("Result from Volatility index", JSON.stringify(result));
         const { longName, symbol, regularMarketPrice, previousClose, regularMarketDayLow, regularMarketDayHigh, fiftyTwoWeekLow, fiftyTwoWeekHigh } = result.chart.result[0].meta;
 
         return `The ${longName} (${symbol}) is currently trading at $${regularMarketPrice}, ${regularMarketPrice > previousClose ? 'up' : 'down'} from its previous close of $${previousClose}. Today's trading range is between $${regularMarketDayLow} and $${regularMarketDayHigh}. Over the past 52 weeks, the VIX has ranged from $${fiftyTwoWeekLow} to $${fiftyTwoWeekHigh}.`;
@@ -41,7 +41,7 @@ async function getStockMarketCurrentPrice(marketSymbol) {
         }
 
         const result = await response.json();
-        console.log("Result from Stock market price", JSON.stringify(result));
+        //console.log("Result from Stock market price", JSON.stringify(result));
         const { longName, symbol, regularMarketPrice, previousClose, regularMarketDayLow, regularMarketDayHigh, fiftyTwoWeekLow, fiftyTwoWeekHigh } = result.chart.result[0].meta;
 
         return `The ${longName} (${symbol}) is currently trading at $${regularMarketPrice}, ${regularMarketPrice > previousClose ? 'up' : 'down'} from its previous close of $${previousClose}. Today's trading range is between $${regularMarketDayLow} and $${regularMarketDayHigh}. Over the past 52 weeks, the SPY has ranged from $${fiftyTwoWeekLow} to $${fiftyTwoWeekHigh}.`;
